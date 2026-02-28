d21274257e8a069afa9504c7dfb834a0e8ed1c50
ydnAkif <akifaydin52@gmail.com>
2026-02-28 17:47:27 +0300

    Initial Commit

---
+++ KingGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KingGame
+//
+//  Created by Akif AYDIN on 28.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KingGame/KingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  KingGameApp.swift
+//  KingGame
+//
+//  Created by Akif AYDIN on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct KingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
